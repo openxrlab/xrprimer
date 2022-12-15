5906f7c7d8e8c7113f132964ef6590b6891ced5f
yl-1993 <yangleidev@gmail.com>
2022-12-15 21:33:55 +0800

    Build doxygen with logo and version

--- docs/en/build_docs.py
+++ docs/en/build_docs.py
@@ -4,6 +4,15 @@ import subprocess
 import sys
 
 
+def _parse_version(version_file='../../version.txt'):
+    with open(version_file) as f:
+        version_lst = []
+        for x in f.readlines():
+            version_lst += [x.strip().split(' ')[-1]]
+        version = '.'.join(version_lst)
+        return version
+
+
 def build_sphinx_docs(temp_dir='_build'):
     """Build sphinx docs for python."""
     if os.path.exists(temp_dir):
@@ -15,8 +24,12 @@ def build_sphinx_docs(temp_dir='_build'):
 
 def build_doxygen_docs(temp_dir='doxygen', cpp_dir='cpp_api'):
     """Build sphinx docs for C++"""
-    cmd = ['doxygen', 'Doxyfile']
-    subprocess.check_call(cmd, stdout=sys.stdout, stderr=sys.stderr)
+    cmd = ['doxygen', 'Doxyfile.in']
+    subprocess.check_call(
+        cmd,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        env=dict(os.environ, PROJECT_NUMBER=_parse_version()))
     # move generated results to _build
     doxygen_dir = os.path.join(temp_dir, 'html')
     dst_dir = os.path.join('_build', 'html', cpp_dir)
