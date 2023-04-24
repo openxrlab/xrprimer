1a471be99215559e80ce483ade7d140e15173b16
gaoyang3 <gaoyang3@sensetime.com>
2023-04-24 12:11:29 +0800

    use ARG for mmcv version control

--- dockerfiles/publish_manylinux/build_publish_docker.sh
+++ dockerfiles/publish_manylinux/build_publish_docker.sh
@@ -1,4 +1,4 @@
 # build according to Dockerfile
 
 TAG="openxrlab/xrprimer_ci:manylinux2014_x86_64_torch1110_mmcv170"
-docker build -t $TAG -f dockerfiles/publish_manylinux/Dockerfile --progress=plain .
+docker build -t $TAG -f dockerfiles/publish_manylinux/Dockerfile --build-arg MMCV_VER=1.7.0 --progress=plain .
