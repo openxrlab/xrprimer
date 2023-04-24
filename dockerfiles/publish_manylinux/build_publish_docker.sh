c733dbfe4b048424977a150bbe0cd4abb9abe987
gaoyang3 <gaoyang3@sensetime.com>
2023-04-24 16:25:17 +0800

    temp commit before switch dev machine

--- dockerfiles/publish_manylinux/build_publish_docker.sh
+++ dockerfiles/publish_manylinux/build_publish_docker.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+MMCV_VER=1.7.0
+TORCH_VER=1.11.0
+TORCHV_VER=0.12.0
+MMCV_VER_DIGIT=${MMCV_VER//./}
+TORCH_VER_DIGIT=${TORCH_VER//./}
+TAG="openxrlab/xrprimer_ci:manylinux2014_x86_64_torch${TORCH_VER_DIGIT}_mmcv${MMCV_VER_DIGIT}"
+echo "tag to build: $TAG"
+BUILD_ARGS="--build-arg MMCV_VER=${MMCV_VER} --build-arg TORCH_VER=${TORCH_VER} --build-arg TORCHV_VER=${TORCHV_VER}"
 # build according to Dockerfile
-
-TAG="openxrlab/xrprimer_ci:manylinux2014_x86_64_torch1110_mmcv170"
-docker build -t $TAG -f dockerfiles/publish_manylinux/Dockerfile --build-arg MMCV_VER=1.7.0 --progress=plain .
+docker build -t $TAG -f dockerfiles/publish_manylinux/Dockerfile $BUILD_ARGS --progress=plain .
